a9591f1077a2957ca1cbc570c900fe6673872195
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-24 12:21:07 -0400

    word

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Add Code to 
-const str = "hello";
-console.log(str.length);
+const word = "hello";
+console.log(word.length);
 // let word = "hello";
 // function name(word) {
 //     let countit = 0;
